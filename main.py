8ec1f4cf272c558024b7927003cb51fe6b2aac9d
Jonwodi <>
2021-04-24 16:19:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# x = 5
+# x = 15
+# y = 7
+# if x  > y:
+#   print("yes")
+
+  # ========================================
+
+# x = 5
+# y = 7
+# if x < y:
+#   print("no")
+
+#   if "hello" in "hello world":
+#     print("yes")
+
+# ========================================
+
+# x = 15
+# x = 5
+# x = 7
+# y = 7
+# if x > y:
+#   print("yes it is greater than")
+# elif x < y:
+#     print("no it is less than")
+# else:
+#       print("neither")
+
+# ========================================
+
+"""
+Nested if statements
+"""
+
+# x = 3
+# x = -3
+# x = 0
+# if x >= 0:
+#   if x == 0:
+#     print("zero")
+#   else:
+#     print("positive number")
+# else:
+#   print("negative number")
+
+"""
+one line if statements
+"""
+
+# if 7 > 5: print("yes")
+
+# if 7 > 5:
+#   print("yes")
+
+# ========================================
+
+"""
+tenary operator
+"""
+
+# print("yes") if 7 > 5 else print("no")
+
+# if 7 > 5:
+#   print("yes")
+# else:
+#   print("no")
+
+# ========================================
+
+# if 100 > 50:
+  # pass
+
